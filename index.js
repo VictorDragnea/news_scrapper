196f30933cc9f19429e606f531aefbf81cc304c0
Victor Dragnea <v.dragnea@mobizcorp.de>
2022-04-19 18:27:11 +0300

    modified port env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const helpers = require('./public/js/helpers');
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const RESULTS_LIMIT = 50;
 
 const app = express();
